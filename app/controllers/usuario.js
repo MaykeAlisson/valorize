3b21efcdc31e885bdcd7e295003cfc2f0a9d477f
Mayke Alisson <maykealison@gmail.com>
2019-11-07 05:09:18 -0300

    Cadastra usuario

--- app/controllers/usuario.js
+++ app/controllers/usuario.js
@@ -2,73 +2,74 @@ module.exports = {
 
   cadastro(app, req, res) {
 
-    // const usuario = {
-    //   nome: req.body.nome,
-    //   sexo: req.body.sexo,
-    //   nascimento: req.body.nascimento,
-    //   email: req.body.email,
-    //   senha: req.body.senha
-    // };
-
     const usuario = req.body;
 
-    // try {
-        const connection = app.app.persistencia.connectionFactory();
-           const usuarioDAO = new app.app.persistencia.UsuarioDAO(connection);
-       // Id do usuario que acaba de ser inserido
-      usuarioDAO.cadastro(usuario, function(erro, resultado){
-        if (erro){
-          res.status(500).json(erro);
-        }
-        console.log("retonrno query");
-        res.status(201).json(resultado);
-      });
-      // res.status(201);
-    //  }catch (e) {
-    //   res.status(500).json(e);
-    // }
+    const connection = app.app.persistencia.connectionFactory();
+    const usuarioDAO = new app.app.persistencia.UsuarioDAO(connection);
+    // Id do usuario que acaba de ser inserido
+    usuarioDAO.cadastro(usuario, function (erro, resultado) {
+      if (erro) {
+        res.status(500).json(erro);
+      }
+      res.status(201);
+    });
 
   },
 
-  login(app, req, res){
+  login(app, req, res) {
 
-    try {
-      const usuario = app.app.model.usuario.login(req.body);
+    const email = req.body.email;
+    const senha = req.body.senha;
 
-      if (usuario === null){
+    const connection = app.app.persistencia.connectionFactory();
+    const usuarioDAO = new app.app.persistencia.UsuarioDAO(connection);
+
+    usuarioDAO.login(email, senha, function (erro, resultado) {
+      if (erro) {
+        res.status(500).send(erro);
+      }
+
+      console.log(resultado);
+
+      const usuario = {
+        "id": resultado[0].id,
+        "nome": resultado[0].nome
+      };
+
+      if (usuario === null) {
         res.status(400).json({
           success: false,
           message: 'Autenticação do Usuário falhou. E-mail ou Senha incorreta!'
         });
-      }else{
+      } else {
         require("dotenv-safe").config();
         const jwt = require('jsonwebtoken');
         const token = jwt.sign({usuario}, process.env.SECRET, {
           expiresIn: '6h'
         });
 
-        const response = {
-          "idUser": usuario.id,
-          "userName": usuario.nome,
-          "token": token
+         const response = {
+           "idUser": usuario.id,
+           "userName": usuario.nome,
+           "token": token
         };
 
         res.status(200).json(response);
       }
-    }catch (e) {
-      res.status(500).json(e);
-    }
+
+    });
+
 
   },
 
-  atualiza(app, req, res){
+  atualiza(app, req, res) {
 
     const usuario = req.body;
 
     try {
       app.app.model.usuario.atualiza(usuario);
       res.status(200);
-    }catch (e) {
+    } catch (e) {
       res.status(500).json(e);
     }
 
--- app/persistencia/UsuarioDAO.js
+++ app/persistencia/UsuarioDAO.js
@@ -1,38 +1,41 @@
-function UsuarioDAO(connection){
-  console.log("to no usuario dao");
+function UsuarioDAO(connection) {
   this._connection = connection;
 }
 
-UsuarioDAO.prototype.cadastro = function(usuario, callback){
-  console.log("UsuarioDAO" + usuario);
-    this._connection.query('INSERT INTO usuario SET ? ', usuario, callback);
-  };
+const mysql = require('mysql');
 
-  // login(usuario){
-  //
-  //   const email = usuario["email"];
-  //   const senha = usuario["senha"];
-  //   const emailBanco = "maykealison@gmail.com";
-  //   const senhaBanco = "12345";
-  //   // verifica se usuario e senha correto no banco
-  //   if(email === emailBanco && senha === senhaBanco){
-  //     const dadosUsuario = {
-  //       id: 1,
-  //       nome: 'Mayke Alisson',
-  //     };
-  //
-  //     return dadosUsuario;
-  //   }
-  //
-  //   return null
-  //
-  // },
-  //
-  // atualiza(usuario){
-  //
-  //   // atualiza o usuario
-  //
-  // },
+UsuarioDAO.prototype.cadastro = function (usuario, callback) {
+  this._connection.query('INSERT INTO usuario SET ? ', usuario, callback);
+};
+
+UsuarioDAO.prototype.login = function (email, senha , callback){
+  this._connection.query('SELECT id, nome FROM usuario WHERE email = ' + mysql.escape(email) + ' AND senha = ' + mysql.escape(senha) + ' ', callback);
+};
+// login(usuario){
+//
+//   const email = usuario["email"];
+//   const senha = usuario["senha"];
+//   const emailBanco = "maykealison@gmail.com";
+//   const senhaBanco = "12345";
+//   // verifica se usuario e senha correto no banco
+//   if(email === emailBanco && senha === senhaBanco){
+//     const dadosUsuario = {
+//       id: 1,
+//       nome: 'Mayke Alisson',
+//     };
+//
+//     return dadosUsuario;
+//   }
+//
+//   return null
+//
+// },
+//
+// atualiza(usuario){
+//
+//   // atualiza o usuario
+//
+// },
 
 
 module.exports = function () {
