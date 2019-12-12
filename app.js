dd2477be95d4d88e121adf04fb7aac86ff723bc6
Mayke Alisson <maykealison@gmail.com>
2019-12-12 04:20:48 -0300

    add log umbler

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const port = 3000;
 
 // Config servidor
 app.listen(port, function () {
-  console.log(`Servidor online na porta: ${port}`);
+  // console.log(`Servidor online na porta: ${port}`);
+  console.log('Umbler listening on port %s', port);
 });
+
