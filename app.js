0f6f9dc9b70ba1ac2d94c415537cb117745f24c7
Mayke Alisson <mayke.furtado@arcom.com.br>
2019-12-24 10:40:18 -0300

    (Feature) Criando variavel de ambiente PORT (Bugfix) Removendo codigo comentado

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //  Importando server.js
 const app = require('./config/server');
-const port = 9000;
+const port = process.env.PORT || 3000;
 
 // Config servidor
 app.listen(port, function () {
-  // console.log(`Servidor online na porta: ${port}`);
   console.log('Umbler listening on port %s', port);
 });
 
